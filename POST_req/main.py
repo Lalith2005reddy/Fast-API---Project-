9752ddab981e9a199ae2865ab690478fe0f0ebec
Lalith2005reddy <lalithreddyarani@gmail.com>
2026-02-20 23:20:01 +0530

    Validations Updated

--- POST_req/main.py
+++ POST_req/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI, Path, Query, HTTPException
-from pydantic import BaseModel
+from typing import Annotated, Literal
+from pydantic import BaseModel,Field, field_validator
 import json
 
 app = FastAPI()
 
 class Patient(BaseModel):
-    name : int
+
+    id : Annotated[str, Field(..., description='ID of patient', examples=['P001'])]
+    name : str
+    city : str
+    age : int
+    gender : str
+    height : float
+    weight : float
 
 def load_data():
     with open('patients.json', 'r') as f:
